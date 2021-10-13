3a23171efdffb38e23603c3b48d2425d888243b9
nisrinawafaz <89191583+nisrinawafaz@users.noreply.github.com>
2021-10-13 15:53:37 +0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :districts, only: [:index, :show]
   resources :statuses, only: [:index, :show]
-  resources :users, only: [:index, :show]
+  resources :users
   resources :line_items
   resources :carts
   resources :sessions, only: [:create]
