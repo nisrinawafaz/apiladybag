fa8bf731eb44c16434c4c0daabd1df78e9702dc1
nisrinawafaz <nisrinawafaz@gmail.com>
2021-10-13 10:48:50 +0700

    deploy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 
   Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://d2fe-203-190-54-83.ngrok.io'
+      origins 'http://9ac7-203-190-54-83.ngrok.io'
       resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :options, :delete], :credentials => true
     end
     end
