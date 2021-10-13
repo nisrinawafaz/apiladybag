6290f20ebe0233e2fbc8f224beff618fbcb4291e
nisrinawafaz <nisrinawafaz@gmail.com>
2021-10-13 14:14:42 +0700

    desploy github

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 
   Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://9ac7-203-190-54-83.ngrok.io'
+      origins 'http://7e2d-203-190-54-83.ngrok.io'
       resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :options, :delete], :credentials => true
     end
     end
