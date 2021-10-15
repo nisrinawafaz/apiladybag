8c79ba2251364e494b16932f7074ce9396a345c4
nisrinawafaz <nisrinawafaz@gmail.com>
2021-10-15 11:07:50 +0700

    push all

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
     end
 
     def update
-<<<<<<< HEAD
       @user = User.find(params[:id])
       if @user.update(user_params)
         render json: {status: :ok}
@@ -29,24 +28,4 @@ class UsersController < ApplicationController
       def user_params
         params.require(:user).permit(:phone, :address)
       end
-=======
-        @user = User.find(params[:id])
-        if @user.update(user_params)
-          render json: {status: :ok}
-        else
-          render json: { status: :unprocessable_entity }
-        end
-      end
-
-      private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:phone, :address)
-    end
->>>>>>> fdea21b380fc28783a340fc4063a76e088b7eef4
 end
