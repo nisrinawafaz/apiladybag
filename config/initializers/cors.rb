777a7b364340c79443fa04a782778507a540c172
nisrinawafaz <nisrinawafaz@gmail.com>
2021-10-19 16:28:28 +0700

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 
   Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://83b1-203-190-54-83.ngrok.io'
+      origins 'https://ladybag.herokuapp.com/'
       resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :options, :delete], :credentials => true
     end
     end
